7491ddef2a768b162f162a7a1de758cb8256df21
soichi-jifuku <soichi-jifuku@hands-lab.com>
2018-05-07 14:30:07 +0900

    localstorage追加

--- js/index.js
+++ js/index.js
@@ -1,39 +1,62 @@
 $(function () {
-    const todo = $('.todo-list');
-    const comp = $('.comp-list');
-    let counter = 0;
+    let todo = $('.todo-list');
+    let comp = $('.comp-list');
+    let indexcounter = 0;
+    let todolist = [];
+    let storage = localStorage.todo;
+
+    if (storage){
+        JSON.parse(storage).forEach( evt => {
+            taskAdd(evt.val);
+            indexcounter++;
+            console.log('indexcounter:'+indexcounter);
+        });
+    }
 
     $('.add-btn').on('click', () => {
-        const addedTaskEl = $('<li><input type="checkbox"><label>');
-        addedTaskEl.addClass();
-
         //入力されたタスクの追加。
         const addTask = $('.new-task').val();
         if (!addTask){
-           $('#new-task').addClass('form-control-danger');
-           $('#new-task').removeClass('comp-btn');
+            $('#new-task').addClass('form-control-danger');
+            $('#new-task').removeClass('comp-btn');
             return;
         }//↑空白処理
         const escape = htmlspecialchars(addTask);
-        counter += 1;
 
+        taskAdd(escape);
+        $('.new-task').val('');
+        indexcounter++;
+        storages();   
+    });
+
+    // =======================================
+    // taskAdd(task) Todo追加
+    // =======================================
+    function taskAdd(task){
+        const addedTaskEl = $('<li class="list-item">').attr('data-todo', indexcounter);
         //削除・コンプ ボタンの追加
         addedTaskEl.html(`
-            ${escape} 
-            <button class="comp-btn">完了</button>
-            <button class="edit-btn">編集</button>
-            <button class="del-btn">削除</button>
+            <input type="checkbox" id="check">
+            <label for="check" id="text">${task}</label>
+            <input type="text">
+            <div class="btn-group btn-group-lg" role="group" aria-label="...">
+                <button class="comp-btn">完了</button>
+                <button class="del-btn">削除</button>
+            </div>
         `
         );
 
-        // 削除ボタン 機能の追加
-        $(addedTaskEl).on('click', '.del-btn', (evt) =>{
-            $(evt.currentTarget).parent().remove();
+        // console.log(task);
+
+          // 削除ボタン 機能の追加
+          $(addedTaskEl).on('click', '.del-btn', (evt) =>{
+            $(evt.currentTarget).parent().parent().remove();
+
         });
 
         // コンプボタン 機能の追加
         $(addedTaskEl).on('click', '.comp-btn', (evt) =>{
-            comp.append($(evt.currentTarget).parent());
+            comp.append($(evt.currentTarget).parent().parent());
             //addedTaskE.find('.comp-btn')打つのめんどいから。
             const changeBtn = addedTaskEl.find('.comp-btn');
             changeBtn.text('戻す');
@@ -43,21 +66,37 @@ $(function () {
 
         // バックボタン 機能の追加
         $(addedTaskEl).on('click', '.back-btn', (evt) =>{
-            todo.append($(evt.currentTarget).parent());
+            todo.append($(evt.currentTarget).parent().parent());
             //addedTaskE.find('.back-btn')打つのめんどいから。
             const changeBtn = addedTaskEl.find('.back-btn');
+
             changeBtn.text('完了');
             changeBtn.addClass('comp-btn');
             changeBtn.removeClass('back-btn');
         })
 
-        //タスクの追加
+         //タスクの追加
         todo.append(addedTaskEl);
-        $('.new-task').val('');
-    });
+    };
 
+    // =======================================
+    // storages() localStorage追加
+    // =======================================
+    function storages(){
+        let list = [];
+
+        todo.find("li").each(function(){
+            let item = $(this);
+            console.log(this);
+
+            list.push({
+                val:item.find('#text').text()
+
+            });
+        });
+        localStorage["todo"] = JSON.stringify(list);
+    }
 });
-
 
 function htmlspecialchars(str) {
 	return (str + '').replace(/&/g, '&amp;')
@@ -66,4 +105,3 @@ function htmlspecialchars(str) {
 		.replace(/</g, '&lt;')
 		.replace(/>/g, '&gt;');
 }
-
